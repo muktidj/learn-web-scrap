d7f10fc7d62492afc7b0c247df1f962ae49bbd7a
Mukti DJ <muktidwijatmoko@gmail.com>
2023-12-13 15:39:29 +0700

    While Break

--- bag_1/basic2.py
+++ bag_1/basic2.py
@@ -5,3 +5,15 @@ for list_array in RANDOMARRAY:
           print(list_array/2)
      except:
           print("the element is not number")
+
+
+
+n=30
+while n>0:
+     print(n)
+     n = n-2
+     if n==10:
+          break
+
+
+print("exit")
