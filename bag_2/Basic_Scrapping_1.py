87c2c1cf328c6c44165548381f0daef1ed7df2ad
Mukti DJ <muktidwijatmoko@gmail.com>
2023-12-14 20:12:01 +0700

    Dapat menggunakan untuk mencari HTML TRE Seperti ini

---
+++ bag_2/Basic_Scrapping_1.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+TARGET_URL = "https://subslikescript.com/movie/SOS_Titanic-79836"
+HEADERS = {
+     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
+}
+RESPONSE = requests.get(TARGET_URL)
+CONTENT = RESPONSE.text
+
+SOUP = BeautifulSoup(CONTENT, "lxml")
+
+ARTICLE_H1 = SOUP.find("article", class_ ="main-article").find("h1")
+
+# ARTICLE_H1_TITLE = ARTICLE_H1.find("h1").get_text()
+print(ARTICLE_H1)
