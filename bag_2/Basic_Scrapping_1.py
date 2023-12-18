e9f3e28733a3902d377e52e55fcd9510fcac943e
Mukti DJ <muktidwijatmoko@gmail.com>
2023-12-18 22:12:47 +0700

    Get Multiple Link

--- bag_2/Basic_Scrapping_1.py
+++ bag_2/Basic_Scrapping_1.py
@@ -15,7 +15,7 @@ SOUP = BeautifulSoup(CONTENT, "lxml")
 # Bisa menggunakan cara seperti ini untuk html bersarang 
 box = SOUP.find("article", class_ ="main-article")
 
-title = box.find("h1").get_text()
+title = box.find("h1").get_text() #type: ignore
 
 transcript = box.find("div", class_ = "full-script").get_text(strip=True, separator=' ') #type: ignore
 print(title) # type: ignore
