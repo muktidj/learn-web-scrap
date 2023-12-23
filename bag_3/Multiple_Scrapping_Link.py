845ce26bc6cf180a1143b7a3de085684192604e7
Mukti DJ <muktidwijatmoko@gmail.com>
2023-12-23 10:18:12 +0700

    Multiple Link and Pagination

--- bag_3/Multiple_Scrapping_Link.py
+++ bag_3/Multiple_Scrapping_Link.py
@@ -1,36 +1,61 @@
 from bs4 import BeautifulSoup
 import requests
 
-ROOT = "https://subslikescript.com"
-TARGET_URL = f"{ROOT}/movies"
-HEADERS = {
-     "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
-}
-RESPONSE = requests.get(TARGET_URL)
-CONTENT = RESPONSE.text
+#####################################################
+# Extracting links from pagination bar
+#####################################################
 
-SOUP = BeautifulSoup(CONTENT, "lxml")
+# How To Get The HTML
+
+ROOT = 'https://subslikescript.com'  # this is the homepage of the website
+WEBSITE = f'{ROOT}/movies_letter-W'  # concatenating the homepage with the movies "letter-X" section. You can choose any section (e.g., letter-A, letter-B, ...)
+RESULT = requests.get(WEBSITE)
+CONTENT = RESULT.text
+SOUP = BeautifulSoup(CONTENT, 'lxml')
 
 
-# Bisa menggunakan cara seperti ini untuk html bersarang 
-box = SOUP.find("article", class_ ="main-article")
+PAGINATION =SOUP.find('ul', class_='pagination')
+PAGES = PAGINATION.find_all('li', class_='page-item') #type: ignore
+LAST_PAGE = PAGES[-2].text # this is the number of pages that the website has inside the movies "letter X" section
 
-links = []
-for link in box.find_all("a", href=True):  # type: ignore
-     links.append(link["href"]) 
-
-print(links)
-
-for link in links:
-     TARGET_URL = f"{ROOT}/{link}"
-     RESPONSE = requests.get(TARGET_URL)
-     CONTENT = RESPONSE.text
-     SOUP = BeautifulSoup(CONTENT, "lxml")
-     BOX = SOUP.find("article", class_ ="main-article")
-     TITLE = BOX.find("h1").get_text() #type: ignore
-     TRANSCRIPT = BOX.find("div", class_ = "full-script").get_text(strip=True, separator=' ') #type: ignore
-     with open(f"{TITLE}.txt", "w") as file:
-          file.write(TRANSCRIPT)
+folder_path = '/Users/user/Learn/python/webscraping/udemy/bag_3/result_scrapping_pagination/'
 
 
 
+for PAGE in range(1, int(LAST_PAGE)+1)[:2]:
+    RESULT = requests.get(f'{WEBSITE}?page={PAGE}')  # structure --> https://subslikescript.com/movies_letter-X?page=2
+    CONTENT = RESULT.text
+    SOUP = BeautifulSoup(CONTENT, 'lxml')
+
+    # Locate the box that contains a list of movies
+    BOX = SOUP.find('article', class_='main-article')
+
+    # Store each link in "links" list (href doesn't consider root aka "homepage", so we have to concatenate it later)
+    links = []
+    for link in BOX.find_all('a', href=True):  # find_all returns a list # type: ignore
+        links.append(link['href'])
+
+    #################################################
+    # Extracting the movie transcript
+    #################################################
+
+
+
+    for link in links:
+        try:  # "try the code below. if something goes wrong, go to the "except" block"
+            RESULT = requests.get(f'{ROOT}/{link}')  # structure --> https://subslikescript.com/movie/X-Men_2-290334
+            CONTENT = RESULT.text
+            SOUP = BeautifulSoup(CONTENT, 'lxml')
+
+            # Locate the box that contains title and transcript
+            BOX =SOUP.find('article', class_='main-article')
+            # Locate title and transcript
+            TITLE = BOX.find('h1').get_text() #type: ignore
+            TRANSCRIPT = BOX.find('div', class_='full-script').get_text(strip=True, separator=' ') #type: ignore
+
+            # Exporting data in a text file with the "title" name
+            with open(f'{folder_path}/{TITLE}.txt', 'w') as file:
+                file.write(TRANSCRIPT)
+        except:
+            print('------ Link not working -------')
+            print(link)
