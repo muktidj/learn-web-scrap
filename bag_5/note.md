55988e513c39639b68b4f51a989fba50f75f9ab9
Mukti DJ <muktidwijatmoko@gmail.com>
2023-12-25 17:20:09 +0700

    Using XPATH for click button

---
+++ bag_5/note.md
@@ -0,0 +1,4 @@
+## Using Search xpath in Chrome Developer Tool
+Press F12 to open Chrome Developer Tool
+In "Elements" panel, press Ctrl+F
+In the search box, type in XPath or CSS Selector, if elements are found, they will be highlighted in yellow.
--- bag_5/pratice_with_selenium.py
+++ bag_5/pratice_with_selenium.py
@@ -1,12 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from time import sleep
+
 
 driver = webdriver.Chrome()
 driver.get("https://www.adamchoi.co.uk/overs/detailed")
 
 
 
-sleep(100)
+ # Find the element using Selenium 4's find_element method
+element = driver.find_element(By.XPATH, "//label[@analytics-event='All matches']") #type: ignore
+
+# Perform actions on the found element (e.g., click, get text, etc.)
+element.click()  # Example: Click the element
+
+
+
+sleep(10000)
 
 driver.quit()
 
