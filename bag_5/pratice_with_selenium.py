ab8cdad1fd2d336321bfd24fa485c52585de3e19
Mukti DJ <muktidwijatmoko@gmail.com>
2023-12-25 18:07:36 +0700

    Extracting data from a table

--- bag_5/pratice_with_selenium.py
+++ bag_5/pratice_with_selenium.py
@@ -15,10 +15,21 @@ element = driver.find_element(By.XPATH, "//label[@analytics-event='All matches']
 element.click()  # Example: Click the element
 
 matches = driver.find_elements(By.TAG_NAME, "tr")
-data = 50
-for match in matches[:data]:
-     print(match.text)
 
+data = 50
+date = []
+home_team = []
+score = []
+away_team = []
+
+for match in matches[:data]:
+     date.append(match.find_element(By.XPATH, "./td[1]").text) #//tr/td[1] 
+     home_team.append(match.find_element(By.XPATH, "./td[2]").text) #//tr/td[2] 
+     # match.find_element(By.XPATH, "./td[3]") #//tr/td[3] 
+     # match.find_element(By.XPATH, "./td[4]") #//tr/td[4]
+
+print(date)
+print(home_team)
 
 sleep(10000)
 
